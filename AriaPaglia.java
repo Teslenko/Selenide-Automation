0abe6e7c77de978e45a3a483533e2a84e39e3f65
Teslenko <Teslvova@gmail.com>
2017-07-20 17:59:11 +0300

    Update AriaPaglia.java

--- AriaPaglia.java
+++ AriaPaglia.java
@@ -1,6 +1,6 @@
-// Тэст вводит слово в гугл, затем открывает по очереди каждую найденную ссылку и ищет в ней
-// текст - "openHAB 2 inside a Docker Container".  Метод - LocatorLink(); собирает основные ссылки "а" из результатов 
-// поиска в Гугл и показывает их в логах.
+//Test introduces the word to GoogleSearch, then opens each link found in turn and looks in it and search the text - "openHAB 2 inside a Docker Container". 
+
+//Method - LocatorLink (); - Collects the main links "a" from the result  search and shows them in the logs.
 
 import com.codeborne.selenide.*;
 import org.junit.Before;
@@ -10,6 +10,7 @@ import org.junit.rules.TestRule;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
 
 /**
  * Created by vt on 04.07.17.
@@ -24,12 +25,17 @@ public class AriaPaglia {
 
     @Test
     public void setup() throws InterruptedException {
-       open("https://www.google.ru/");
-       $(By.name("q")).val("Docker").pressEnter();Thread.sleep(1000);
-       for ( int t=0; t < 2; t++) {         // обворачиваем весь тест в облочку цикла
+
+        clearBrowserCache();
+        open("https://www.google.ru/");
+        $(By.name("q")).val("Docker").pressEnter(); Thread.sleep(500);
+       for ( int t=0; t < 2; t++) {                                 // обворачиваем весь тест в облочку цикла
+
            elementCollection(); //метод корый открывает по очереди каждый найденный результат и проверяет в нем элемент
-           locatorlink();       // метод который собирает все ссылки
-           $(By.id("pnnext")).click(); Thread.sleep(700);                           // кликаем на кнопку "след страница"
+
+           locatorlink();                                            // метод который собирает все ссылки
+
+           $(By.id("pnnext")).click(); Thread.sleep(700);            // кликаем на кнопку "след страница"
        }
     }
 
@@ -39,7 +45,7 @@ public class AriaPaglia {
 
         for (SelenideElement elem : links) {                         // представление каждого элемента к массиву
             elem.click();                                            // кликаем по каждой найденой Теме
-            switchTo().window(1);     Thread.sleep(3000);            // переходим в новую вкладку
+            switchTo().window(1); Thread.sleep(2000);                // переходим в новую вкладку
 
             ifMetod();
 
@@ -48,7 +54,9 @@ public class AriaPaglia {
 
     @Test
     public void ifMetod() throws InterruptedException{
+
         SelenideElement locator = $(byText("openHAB 2 inside a Docker Container"));
+
         if (locator.exists()){                                       // Пытаемся найти в открытой вкладке "Текст"
             locator.scrollTo().contextClick(); Thread.sleep(3000);   // Если условие выполнилось, то жмем на "Текст"
         }
@@ -58,6 +66,7 @@ public class AriaPaglia {
 
     @Test
     public void locatorlink() throws InterruptedException {
+
         ElementsCollection link = $$("div.g>div>div.rc>h3.r>a");
         for (int i = 0; i<10; i++) {
             System.out.println(link.get(i).getAttribute("text"));
@@ -75,5 +84,4 @@ public class AriaPaglia {
         System.setProperty(browserPropertyName, browserPropertyPath);
         System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tT %4$s %5$s%6$s%n");
     }
-
 }
