34fa81519c1b67be57bef063ec4b316ff2584685
Teslenko <Teslvova@gmail.com>
2017-07-20 16:53:38 +0300

    Update AriaPaglia.java

--- AriaPaglia.java
+++ AriaPaglia.java
@@ -16,42 +16,44 @@ import static com.codeborne.selenide.Selenide.*;
  */
 public class AriaPaglia {
 
+        public static boolean holdBrowserOpen = Boolean.getBoolean("selenide.holdBrowserOpen"); // Dselenide property
         final String browserName = "chrome";
         final int timeOut = 5000;
         final String browserPropertyName = "webdriver.chrome.driver";
         final String browserPropertyPath = "./src/main/resources/chromedriver";
 
     @Test
-    public void findlocator() throws InterruptedException {
-
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tT %4$s %5$s%6$s%n");
-
-        open("https://www.google.ru/");
-        $(By.name("q")).val("Docker").pressEnter();Thread.sleep(1000);
-
+    public void setup() throws InterruptedException {
+       open("https://www.google.ru/");
+       $(By.name("q")).val("Docker").pressEnter();Thread.sleep(1000);
        for ( int t=0; t < 2; t++) {         // обворачиваем весь тест в облочку цикла
-
-           for (int p = 0; p < 1; p++) {    // обворачиваем основной функционал теста в цикл
-               ElementsCollection links = $$("div.g>div>div.rc>h3.r>a");    // приравниваем к дочерней ссылки
-
-               for (SelenideElement elem : links) {                         // представление каждого элемента к массиву
-                   elem.click();                                            // кликаем по каждой найденой Теме
-                   switchTo().window(1);     Thread.sleep(3000);            // переходим в новую вкладку
-
-                   for ( int q=0; q<1; q++) {
-                       SelenideElement locator = $(byText("openHAB 2 inside a Docker Container"));
-
-                       if (locator.exists()){                                       // Пытаемся найти в открытой вкладке "Текст"
-                           locator.scrollTo().contextClick(); Thread.sleep(3000);   // Если условие выполнилось, то жмем на "Текст"
-                       }
-                       switchTo().window(1).close();                                // если не нашли то закрываем вкладку
-                       switchTo().window(0);            Thread.sleep(700);          // переходим в первую вкладку
-                   }
-               }
-           }
-           locatorlink();                                                           // метод который собирает все ссылки
+           elementCollection(); //метод корый открывает по очереди каждый найденный результат и проверяет в нем элемент
+           locatorlink();       // метод который собирает все ссылки
            $(By.id("pnnext")).click(); Thread.sleep(700);                           // кликаем на кнопку "след страница"
        }
+    }
+
+    @Test
+    public void elementCollection() throws InterruptedException{
+        ElementsCollection links = $$("div.g>div>div.rc>h3.r>a");    // приравниваем к дочерней ссылки
+
+        for (SelenideElement elem : links) {                         // представление каждого элемента к массиву
+            elem.click();                                            // кликаем по каждой найденой Теме
+            switchTo().window(1);     Thread.sleep(3000);            // переходим в новую вкладку
+
+            ifMetod();
+
+        }
+    }
+
+    @Test
+    public void ifMetod() throws InterruptedException{
+        SelenideElement locator = $(byText("openHAB 2 inside a Docker Container"));
+        if (locator.exists()){                                       // Пытаемся найти в открытой вкладке "Текст"
+            locator.scrollTo().contextClick(); Thread.sleep(3000);   // Если условие выполнилось, то жмем на "Текст"
+        }
+        switchTo().window(1).close();                                // если не нашли то закрываем вкладку
+        switchTo().window(0);            Thread.sleep(700);          // переходим в первую вкладку
     }
 
     @Test
@@ -65,10 +67,13 @@ public class AriaPaglia {
 
     @Rule
     public TestRule report = new com.codeborne.selenide.junit.TextReport().onFailedTest(true);
+
     @Before
     public void before(){
         Configuration.browser = browserName;
         Configuration.timeout = timeOut;
         System.setProperty(browserPropertyName, browserPropertyPath);
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tT %4$s %5$s%6$s%n");
     }
+
 }
